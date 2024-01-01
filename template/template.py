8eeb1f64927f00064ce1748fcb688d84c13e12d3
rw <w.bayashi@gmail.com>
2024-01-01 19:01:17 +0900

    update template

--- template/template.py
+++ template/template.py
@@ -5,10 +5,10 @@ def main():
   input = sys.stdin.readline
   # 数値 n を取得する場合
   n = int(input())
-  # 数値 n, x を取得する場合
-  n, x = [int(i) for i in input().replace('\n', '').split(' ')]
   # 文字列 s を取得する場合
   s = input()
+  # 数値 n, x を取得する場合
+  n, x = [int(i) for i in input().replace('\n', '').split(' ')]
   # 1行に n 個ある数値を配列 s として取得する場合
   s = [int(i) for i in input().replace('\n', '').split(' ')]
   # n 行の数値の配列 a を取得する場合
