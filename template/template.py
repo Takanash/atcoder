26784418b7bd16074407a82e63aaa8873b99652e
rw <w.bayashi@gmail.com>
2024-01-06 19:37:37 +0900

    update template

--- template/template.py
+++ template/template.py
@@ -16,6 +16,9 @@ def main():
   s = [int(i) for i in input().replace('\n', '').split(' ')]
   # n 行の数値の配列 a を取得する場合
   a = [input() for i in range(n)]
+  # n 行の `l r` を二次元配列[[l1, r1], ...]として取得する場合 
+  l_r = [map(int, input().replace('\n', '').split(' ')) for i in range(n)]
+
 
 
 # 時間計測用
