8e7381a8e3474faaec896851aba4947ee110fba8
rw <w.bayashi@gmail.com>
2024-01-15 21:18:03 +0900

    update template

--- template/template.py
+++ template/template.py
@@ -37,3 +37,13 @@ if TIME:
   end = time.time()
   time_diff = end - start
   print(f"実行時間: {str(time_diff)}")
+
+
+# 10進数→n進数への変換
+# @param [int] value 変換する値
+# @param [int] base 変換するn進数
+# @return [str] n進数に変換した値
+def base10int(value, base):
+    if (int(value / base)):
+        return base10int(int(value / base), base) + str(value % base)
+    return str(value % base)
