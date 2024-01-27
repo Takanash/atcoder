7cbed09abd4fa81212861c7be5c19b074faa7c81
rw <w.bayashi@gmail.com>
2024-01-27 16:56:39 +0900

    udpate template

--- template/template.py
+++ template/template.py
@@ -13,9 +13,9 @@ def main():
   # 1行に n 個ある数値を配列 s として取得する場合
   s = [int(i) for i in input().replace('\n', '').split(' ')]
   # n 行の数値の配列 a を取得する場合
-  a = [input() for i in range(n)]
+  a = [int(input()) for _ in range(n)]
   # n 行の `l r` を二次元配列[[l1, r1], ...]として取得する場合 
-  l_r = [list(map(int, input().replace('\n', '').split(' '))) for i in range(n)]
+  l_r = [list(map(int, input().replace('\n', '').split(' '))) for _ in range(n)]
 
 
 
